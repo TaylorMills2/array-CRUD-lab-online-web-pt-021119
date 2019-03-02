ac9766470599b7ff7ad239fce0f1f07dd2b49d40
Taylor Mills <taylormills24@yahoo.com>
2019-03-02 22:10:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,8 +17,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
- famous_actors = ["arrays!"]
- actor = famous_actors.pop
+ array.pop
 end
 
 def remove_element_from_start_of_array(array)
